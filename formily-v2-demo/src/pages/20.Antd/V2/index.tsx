a4ad584fbfcf78efc332836238b55b8d6c33b626
xiao.geng <xiao.geng@relxtech.com>
2023-09-21 09:42:05 +0800

    feat: 常规暂存

--- formily-v2-demo/src/pages/20.Antd/V2/index.tsx
+++ formily-v2-demo/src/pages/20.Antd/V2/index.tsx
@@ -2,7 +2,7 @@
  * @description 构建antd最小组件间的关系
  */
 import React from 'react'
-import { FormItem, Space, NumberPicker } from '@formily/antd'
+import { FormItem, Input, Space } from '@formily/antd'
 import { createForm } from '@formily/core'
 import { FormProvider, Field } from '@formily/react'
 
@@ -10,15 +10,19 @@ const form = createForm()
 
 const DemoPage = () => {
   console.log('form:', form)
+  form.getFormGraph();
+
   return (
+    // createContent
     <FormProvider form={form}>
+      <Space>
         <Field
           name="total"
           title="数量"
-          initialValue={101}
-          // decorator={[FormItem]}
+          initialValue={10}
+          decorator={[FormItem]}
           component={[
-            NumberPicker,
+            Input,
             {
               placeholder: '请输入数量',
               style: {
@@ -27,6 +31,30 @@ const DemoPage = () => {
             },
           ]}
         />
+        <Field
+          name="amount"
+          title="总价"
+          disabled
+          initialValue={180}
+          decorator={[FormItem]}
+          reactions={[(field) => {
+             if (field.mounted) {
+              form.fields?.amount?.destroy()
+            }
+            const total = field.query('total')
+            field.value = total.get("value") * 18
+          }]}
+          component={[
+            Input,
+            {
+              placeholder: '请输入价格',
+              style: {
+                width: 300,
+              },
+            },
+          ]}
+        />
+      </Space>
     </FormProvider>
   )
 }
