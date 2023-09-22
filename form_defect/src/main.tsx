f3cb6882246acfaaa0952d7ec2f8ffe325ecb4bb
zhangguofei <feywej@163.com>
2023-09-22 15:39:06 +0800

    自定义FormItem组件。

--- form_defect/src/main.tsx
+++ form_defect/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import Form from './pages/V1/form-demo/index'
 import AntdForm from './pages/V1/antd-demo/index'
 import Formily from './pages/V1/formily-demo/index'
-import FormilyJSX from './pages/V2/index'
+import FormItem from './pages/V2/index'
 import './index.css'
 import { Button, Card, Tabs } from 'antd'
 import stores from './store'
@@ -12,22 +12,18 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider {...stores}>
     <React.StrictMode>
       <Card style={{ width: "660px" }}>
-        <Button onClick={() => {
-          stores.v2Store.increment()
-        }}>周期加</Button>
-        <Button onClick={() => {
-          stores.v2Store.decrement()
-        }}>周期减</Button>
-        <Tabs defaultActiveKey='1'>
+        <Tabs defaultActiveKey='4'>
           <Tabs.TabPane tab="原生form实现" key="1">
             <Form />
           </Tabs.TabPane>
           <Tabs.TabPane tab="antd实现" key="2">
             <AntdForm />
           </Tabs.TabPane>
-          <Tabs.TabPane tab="formily实现" key="3">
-            {/* <Formily /> */}
-            <FormilyJSX />
+          {/* <Tabs.TabPane tab="formily实现" key="3">
+            <Formily />
+          </Tabs.TabPane> */}
+          <Tabs.TabPane tab="自定义FormItem" key="4">
+            <FormItem />
           </Tabs.TabPane>
         </Tabs>
       </Card>
